42380390dc86a64d7097114d4e1156f28d83dc54
Lilia Khazieva <lilia002777@mail.ru>
2023-05-18 19:25:28 +0300

    pr15 add app

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb')
 app.use(express.json());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
